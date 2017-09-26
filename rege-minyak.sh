b80aa46afd8554f9ff32c79f2fdd20d06f7f9489
Muhammad Najmi Ahmad Zabidi <najmi.zabidi@gmail.com>
2017-09-26 23:59:35 +0800

    Added price difference

--- rege-minyak.sh
+++ rege-minyak.sh
@@ -12,3 +12,13 @@ printf "RON97\tRM${minyak[6]}\n"
 printf "RON95\tRM${minyak[7]}\n"
 printf "DIESEL\tRM${minyak[8]}\n"
 echo '-----------------------------------------------------------' 
+printf "\n"
+printf "PERBANDINGAN DENGAN MINGGU LEPAS (tanda negatif bermaksud harga semasa lebih rendah)\n"
+echo '-----------------------------------------------------------' 
+declare -a minyaklama=(`printf "$tabulate\n"|tail -n 7`)
+bezaron97=$(echo "scale=2;${minyak[6]}-${minyaklama[0]}"|bc)
+bezaron95=$(echo "scale=2;${minyak[7]}-${minyaklama[1]}"|bc)
+bezadiesel=$(echo "scale=2;${minyak[8]}-${minyaklama[2]}"|bc)
+printf "Beza RON97 RM $bezaron97\n"
+printf "Beza RON95 RM $bezaron95\n"
+printf "Beza DIESEL RM $bezadiesel\n"
